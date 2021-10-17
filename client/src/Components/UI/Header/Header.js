162ee85f773818d1241fe185e3f318abfdc1fa57
Alam <alam.15@outlook.com>
2021-10-17 15:59:16 -0400

    header

--- client/src/Components/UI/Header/Header.js
+++ client/src/Components/UI/Header/Header.js
@@ -1,14 +1,43 @@
-import React from "react";
-import "./Header.css";
+import { Link } from "react-router-dom";
 
-const Header = (props) => {
+function Header(props) {
   return (
     <header>
-      <div className="container">
-        <h1 className="title">{props.title}</h1>
-      </div>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/">
+            <i class="fas fa-video me-2"></i>
+            Movies
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#nav-top"
+            aria-controls="nav-top"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="nav-top">
+            <ul className="navbar-nav mb-2 mb-lg-0 ms-auto">
+              <li class="nav-item">
+                <Link class="nav-link" activeClassName="active" to="/series">
+                  Series
+                </Link>
+              </li>
+              <li class="nav-item">
+                <Link class="nav-link" activeClassName="active" to="/actors">
+                  Actors
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </header>
   );
-};
+}
 
 export default Header;
