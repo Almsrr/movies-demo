fed10219236df3b3f7547f1a7d646b59430baebe
Alam <alam.15@outlook.com>
2021-10-17 15:58:40 -0400

    layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,20 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 
-import styles from "./App.module.css";
-import Navigation from "./Components/UI/Navigation/Navigation";
-import Header from "./Components/UI/Header/Header";
-import Footer from "./Components/UI/Footer/Footer";
+import Layout from "./Components/UI/Layout";
 import Movies from "./Pages/Movies/Movies";
 import Actors from "./Pages/Actors/Actors";
-import ActorDetails from "./Pages/Actors/ActorDetails/ActorDetails";
+import ActorDetails from "./Pages/ActorDetails/ActorDetails";
 
 function App() {
   return (
-    <Router>
-      <div className={styles.app}>
-        <Navigation />
-        <Header title="Movies" />
-        <main className="container">
-          <Switch>
-            <Route path="/" exact component={Movies} />
-            <Route path="/actors" exact component={Actors} />
-            <Route path="/actors/:id" component={ActorDetails} />
-          </Switch>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <Layout>
+      <Switch>
+        <Route path="/" component={Movies} exact />
+        <Route path="/actors" component={Actors} exact />
+        <Route path="/actors/:actorId" component={ActorDetails} />
+      </Switch>
+    </Layout>
   );
 }
 
