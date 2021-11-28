6f235b135347fe613d20e8a16386cbf64951850f
Alam <alam.15@outlook.com>
2021-11-28 15:46:31 -0400

    modal close button

--- client/src/Components/UI/Modal/Modal.jsx
+++ client/src/Components/UI/Modal/Modal.jsx
@@ -6,7 +6,16 @@ function Modal(props) {
   const modal = (
     <Fragment>
       <div className={styles.overlay} onClick={props.onClose}></div>
-      <div className={styles.modal}>{props.children}</div>
+      <div className={styles.modal}>
+        <button
+          type="button"
+          className={styles.modalCloseBtn}
+          onClick={props.onClose}
+        >
+          <i className="fas fa-times"></i>
+        </button>
+        {props.children}
+      </div>
     </Fragment>
   );
   return ReactDOM.createPortal(modal, document.getElementById("modal-root"));
